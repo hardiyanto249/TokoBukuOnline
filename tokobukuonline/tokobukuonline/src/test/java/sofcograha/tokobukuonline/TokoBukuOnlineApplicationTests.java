ea51657bf48a690fd8f7430ee625029d37a6ac32
Dewi Mira R <Dewi Mira R@LAPTOP-V58651GS>
2022-07-09 12:55:19 +0700

    Init commit.

---
+++ tokobukuonline/tokobukuonline/src/test/java/sofcograha/tokobukuonline/TokoBukuOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package sofcograha.tokobukuonline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TokoBukuOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
