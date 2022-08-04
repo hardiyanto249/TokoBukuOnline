768ee0548bbda5c08aa6a296b79159d15f60ba8c
Yan <hardiyanto.id@gmail.com>
2022-08-04 13:49:44 +0700

    modif filter

--- TokoBukuNet/src/main/java/com/sofco/book/springboot/filter/BukuFilter.java
+++ TokoBukuNet/src/main/java/com/sofco/book/springboot/filter/BukuFilter.java
@@ -20,16 +20,14 @@ public class BukuFilter implements Filter{
 	      ServletRequest servletRequest,
 	      ServletResponse servletResponse,
 	      FilterChain filterChain) throws IOException, ServletException {
-
-	    HttpServletResponse response = (HttpServletResponse) servletResponse;
-	    response.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
-	    response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE, PATCH");
-        response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-        response.setHeader("Access-Control-Expose-Headers", "Location");
-	    filterChain
-	      .doFilter(servletRequest, servletResponse);
-	  }
+		  	HttpServletResponse response = (HttpServletResponse) servletResponse;
+		  	response.setHeader("Access-Control-Allow-Origin", "http://localhost:4200");
+		  	response.setHeader("Access-Control-Allow-Methods", "POST, GET, PUT, OPTIONS, DELETE, PATCH");
+		  	response.setHeader("Access-Control-Max-Age", "3600");
+		  	response.setHeader("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+		  	response.setHeader("Access-Control-Expose-Headers", "Location");
+		  	filterChain.doFilter(servletRequest, servletResponse);
+	  	  }
 	  
 	    @Override
 	    public void init(FilterConfig filterConfig) {}
